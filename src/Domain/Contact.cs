bebce7e959518815aa7da5b79f6a7cf5a30a776f
Tomasz Goluch <goluch@eti.pg.edu.pl>
2024-07-10 12:53:20 +0200

    added Domain and Application projects

---
+++ src/Domain/Contact.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Enums;
+
+namespace Domain
+{
+    public class Contact
+    {
+        public Guid Id { get; set; }
+        public string Forename { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public Category Categoty { get; set; }
+    }
+}
