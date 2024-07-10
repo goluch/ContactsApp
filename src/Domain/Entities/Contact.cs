00fe5f23fef58910e71639ba92eae7c4c6f5289d
Tomasz Goluch <goluch@eti.pg.edu.pl>
2024-07-10 13:09:58 +0200

    added Subcategory prop to Contact entity

--- src/Domain/Entities/Contact.cs
+++ src/Domain/Entities/Contact.cs
@@ -9,7 +9,8 @@ namespace Domain.Entities
         public string Surname { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public Category Categoty { get; set; }
+        public Category Category { get; set; }
+        public string Subcategory { get; set; }
         public int PhoneNumber { get; set; }
         public DateOnly BirthDate { get; set; }
     }
--- src/Domain/Enums/Subcategory.cs
+++ src/Domain/Enums/Subcategory.cs
@@ -1,6 +1,6 @@
 ﻿namespace Domain.Enums
 {
-    public enum SubCategory
+    public enum Subcategory
     {
         None = 0,
         Boss,
