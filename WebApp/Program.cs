d649b70595b0080c4ef3397aea9c612b55698175
Tomasz Goluch <goluch@eti.pg.edu.pl>
2024-07-11 11:14:20 +0200

    added Swagger with temporary controller

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,23 +1,39 @@
+using Microsoft.AspNetCore.Builder;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+
+}
+else
 {
     app.UseExceptionHandler();
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
+app.UseSwagger();
+app.UseSwaggerUI();
 
-app.UseRouting();
+app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseRouting();
+app.MapControllers();
+
+app.MapFallbackToFile("/index.html");
 
 app.Run();
