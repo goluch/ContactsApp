70fbd8911e4fe01c89542d68998a8b532892f9c4
Tomasz Goluch <goluch@eti.pg.edu.pl>
2024-07-29 21:59:47 +0200

    fixed editt contact component

--- src/Client/src/components/ContactDetails.vue
+++ src/Client/src/components/ContactDetails.vue
@@ -3,7 +3,7 @@
         <h3>Contact {{ contact.forename }} {{ contact.surname }} details:</h3>
         <p><em>Email:</em> {{ contact.email }}</p>
         <p><em>Password:</em> {{ contact.password }}</p>
-        <p><em>Category:</em> {{ contact.category.categoryName }}</p>
+        <p><em>Category:</em> {{ contact.category.categoryItemValue.categoryName }}</p>
         <p><em>Subcategory:</em> {{ contact.category.subcategoryName }}</p>
         <p><em>Phone Number:</em> {{ contact.phoneNumber }}</p>
         <p><em>Birth Date:</em> {{ contact.birthDate }}</p>
--- src/Client/src/components/ContactEdit.vue
+++ src/Client/src/components/ContactEdit.vue
@@ -1,37 +1,106 @@
 <template>
     <div v-if="this.$store.state.contactEdit" class="content">
-        <h2>Edit contact</h2>
-        <table>
-            <thead>
-                <tr>
-                    <th>Forename</th>
-                    <th>Surname</th>
-                    <th>Email</th>
-                    <th>Password</th>
-                    <th>Category</th>
-                    <th>Subcategory</th>
-                    <th>Phone Number</th>
-                    <th>Birth Date</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>{{ contact.forename }}</td>
-                    <td>{{ contact.surname }}</td>
-                    <td>{{ contact.email }}</td>
-                    <td>{{ contact.password }}</td>
-                    <td>{{ contact.category.categoryName }}</td>
-                    <td>{{ contact.category.subcategoryName }}</td>
-                    <td>{{ contact.phoneNumber }}</td>
-                    <td>{{ contact.birthDate }}</td>
-                </tr>
-            </tbody>
-        </table>
+        <h3>Add contact</h3>
+        <form @submit.prevent="editContact">
+            <div class="mb-3">
+                <label>Forename:</label>
+                <input type="text" class="form-control" v-model="contact.forename" required>
+            </div>
+            <div class="mb-3">
+                <label>Surname:</label>
+                <input type="text" class="form-control" v-model="contact.surname" required>
+            </div>
+            <div class="mb-3">
+                <label>Email:</label>
+                <input type="email" class="form-control" v-model="contact.email" required>
+            </div>
+            <div class="mb-3">
+                <label>Password:</label>
+                <input type="password" class="form-control" v-model="contact.password" required>
+            </div>
+            <div class="mb-3">
+                <label>Category:</label>
+                <select class="form-control" v-model="contact.category.categoryItemValue" @change="adjustSubcategory" required>
+                    <option v-for="item in this.$store.state.suportedCategoriesList" :value="item">{{ item.categoryName }}</option>
+                </select>
+            </div>
+            <div class="mb-3">
+                <label v-if="showSubcategoryLabel">Subcategory:</label>
+                <input v-if="showSubcategoryInput" type="text" class="form-control" v-model="contact.category.subcategoryName" required>
+                <select v-if="showSubcategorySelect" class="form-control" v-model="contact.category.subcategoryName" required>
+                    <option v-for="item in this.$store.state.suportedBusinessSubcategoriesList" :value="item">{{ item }}</option>
+                </select>
+
+            </div>
+            <div class="mb-3">
+                <label>Phone Number:</label>
+                <input type="tel" class="form-control" v-model="contact.phoneNumber" required>
+            </div>
+            <div class="mb-3">
+                <label>Birth Date:</label>
+                <input type="date" class="form-control" v-model="contact.birthDate" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Add Contact</button>
+        </form>
     </div>
 </template>
 
 <script lang="ts">
+    import { Contact, Category } from "../common/types";
+
     export default {
+        data() {
+            return {
+                showSubcategoryLabel: false,
+                showSubcategoryInput: false,
+                showSubcategorySelect: false,
+                newContact: new Contact,
+            };
+        },
         props: ['contact'],
+        methods: {
+            isSubcategorySelectDisabled() {
+                return false;
+            },
+            adjustSubcategory(e) {
+                switch (this.newContact.category.categoryItemValue.allowedSubcategories) {
+                    case 'Any':
+                        this.showSubcategoryLabel = true;
+                        this.showSubcategoryInput = true;
+                        this.showSubcategorySelect = false;
+                        break;
+                    case 'Restricted':
+                        this.showSubcategoryLabel = true;
+                        this.showSubcategoryInput = false;
+                        this.showSubcategorySelect = true;
+                        break;
+                    case 'None':
+                        this.showSubcategoryLabel = false;
+                        this.showSubcategoryInput = false;
+                        this.showSubcategorySelect = false;
+                        break;
+                }
+            },
+            editContact() {
+                this.$store.commit('setLoading', true);
+                this.$store.commit('setAllDisplaysNull');
+                fetch('CreateContact', {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify(this.newContact),
+                })
+                    .then(response => {
+                        this.$store.commit('setLoading', false);
+                        if (!response.ok) {
+                            this.$store.commit('setMsg', 'Failed to add contact.');
+                            return;
+                        }
+                        else {
+                            this.$store.commit('setMsg', "Contact added successfully.");
+                            return;
+                        }
+                    })
+            },
+        }
     }
 </script>
