9c2bfbd19556f43b692521a4eb6c514c384723f1
Tomasz Goluch <goluch@eti.pg.edu.pl>
2024-07-30 14:32:21 +0200

    Added contact delete component

--- src/Client/src/components/ContactDelete.vue
+++ src/Client/src/components/ContactDelete.vue
@@ -1,37 +1,46 @@
 <template>
     <div v-if="this.$store.state.contactDelete" class="content">
-        <h2>Delete contact</h2>
-        <table>
-            <thead>
-                <tr>
-                    <th>Forename</th>
-                    <th>Surname</th>
-                    <th>Email</th>
-                    <th>Password</th>
-                    <th>Category</th>
-                    <th>Subcategory</th>
-                    <th>Phone Number</th>
-                    <th>Birth Date</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>{{ contact.forename }}</td>
-                    <td>{{ contact.surname }}</td>
-                    <td>{{ contact.email }}</td>
-                    <td>{{ contact.password }}</td>
-                    <td>{{ contact.category.categoryName }}</td>
-                    <td>{{ contact.category.subcategoryName }}</td>
-                    <td>{{ contact.phoneNumber }}</td>
-                    <td>{{ contact.birthDate }}</td>
-                </tr>
-            </tbody>
-        </table>
+        <h3>Contact {{ contact.forename }} {{ contact.surname }} details:</h3>
+        <p><em>Email:</em> {{ contact.email }}</p>
+        <p><em>Password:</em> {{ contact.password }}</p>
+        <p><em>Category:</em> {{ contact.category.categoryItemValue.categoryName }}</p>
+        <p><em>Subcategory:</em> {{ contact.category.subcategoryName }}</p>
+        <p><em>Phone Number:</em> {{ contact.phoneNumber }}</p>
+        <p><em>Birth Date:</em> {{ contact.birthDate }}</p>
+        <button v-on:click="deleteContact" class="btn btn-danger">Delete contact</button>
     </div>
 </template>
 
+<script setup lang="ts">
+    const props = defineProps({
+        contact: Contact
+    })
+</script>
+
 <script lang="ts">
+    import { Contact, Category } from "../common/types";
+
     export default {
-        props: ['contact'],
+        methods: {
+            deleteContact() {
+                this.$store.commit('setLoading', true);
+                this.$store.commit('setAllDisplaysNull');
+                fetch(`DeleteContact/${this.contact.id}`, {
+                    method: 'DELETE',
+                    headers: { 'Content-Type': 'application/json' },
+                })
+                    .then(response => {
+                        this.$store.commit('setLoading', false);
+                        if (!response.ok) {
+                            this.$store.commit('setMsg', 'Failed to delete contact.');
+                            return;
+                        }
+                        else {
+                            this.$store.commit('setMsg', "Contact changes deleted successfully.");
+                            return;
+                        }
+                    })
+            },
+        }
     }
 </script>
