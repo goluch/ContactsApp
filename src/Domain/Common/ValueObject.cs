930f6f80f77362ae50bb401bf5ae045fda465efe
Tomasz Goluch <goluch@eti.pg.edu.pl>
2024-08-03 18:31:23 +0200

    Implemented == and != value base operators

--- src/Domain/Common/ValueObject.cs
+++ src/Domain/Common/ValueObject.cs
@@ -9,6 +9,15 @@ namespace Domain.Common
     // Learn more: https://docs.microsoft.com/en-us/dotnet/standard/microservices-architecture/microservice-ddd-cqrs-patterns/implement-value-objects
     public abstract class ValueObject
     {
+        public static bool operator ==(ValueObject one, ValueObject two)
+        {
+            return EqualOperator(one, two);
+        }
+
+        public static bool operator !=(ValueObject one, ValueObject two)
+        {
+            return NotEqualOperator(one, two);
+        }
         protected static bool EqualOperator(ValueObject left, ValueObject right)
         {
             if (left is null ^ right is null)
